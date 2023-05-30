a011efb3c8e3bdec4c1790566f73d6a84f34394a
Victor Municelli Dario <victor@blockforce.in>
2023-05-30 16:05:52 -0300

    Revert "Merge branch 'feat/webhook-sig'"
    This reverts commit 3293b66d7b85ef0b6c05ce6dcc881932cb133bfe, reversing
    changes made to b5b56ef29f6e147f2c1c57e556e1f949e7314182.

--- includes/class-cryptumcheckout-log.php
+++ includes/class-cryptumcheckout-log.php
@@ -2,27 +2,24 @@
 
 class CryptumCheckout_Log
 {
-	public static function log($prefix, $message, $level = 'info')
+	public static function log($message, $level = 'info')
 	{
 		$log = $message;
 		if (is_array($message) || is_object($message)) {
 			$log = print_r($message, true);
 		}
-		if (empty($prefix)) {
-			$prefix = '[Cryptum Checkout Log]';
-		}
-		$log = $prefix . ': ' . $log;
+		$log = '[Cryptum Checkout Log]: ' . $log;
 		if (function_exists('wc_get_logger')) {
 			wc_get_logger()->log($level,  $log, array('source' => 'cryptum-checkout'));
 		}
 		error_log($log);
 	}
-	public static function info($prefix, $message)
+	public static function info($message)
 	{
-		self::log('[Cryptum Checkout Info]' . $prefix, $message, 'info');
+		self::log($message, 'info');
 	}
-	public static function error($prefix, $message)
+	public static function error($message)
 	{
-		self::log('[Cryptum Checkout Error]' . $prefix, $message, 'error');
+		self::log($message, 'error');
 	}
 }
